ff50efb18e750e0b2027edde4ea5cc3f9b9c952f
smee <maxim.oransky@gmail.com>
2026-05-13 12:52:19 +0200

    vie: add uu base form to digraph_vowels (covers ưu); detect caveat

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="syllabreak",
-    version="0.16.0",
+    version="0.16.1",
     packages=find_packages(),
     install_requires=[
         "PyYAML>=6.0",
